01a7c624be12468ab345c120795309129b41aa19
QueenDebbiethafirst <anuobisesan@gmail.com>
2023-06-22 19:33:57 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
-#include "main.h"
-
+#include <unistd.h>
 
 /**
-* print_most_numbers - print num except 2 and 4
-*
-* return: void
-*/
+ * print_most_numbers - print numbers except 2 and 4
+ *
+ * Return: void
+ */
 void print_most_numbers(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-if (!(c == '2' || c == '4'))
-_putchar(c);
-}
-_putchar('\n');
+    char c;
+
+    for (c = '0'; c <= '9'; c++)
+    {
+        if (!(c == '2' || c == '4'))
+            write(1, &c, 1); /* Use the write system call to print a single character */
+    }
+    write(1, "\n", 1); /* Use the write system call to print a newline character */
 }
