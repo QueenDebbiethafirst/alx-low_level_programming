d9c5b459e8d276b1876aa6b090096d733ff35412
QueenDebbiethafirst <anuobisesan@gmail.com>
2023-07-07 16:13:02 -0700

    static lib

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+_puts("\"Programming is like building a multilingual puzzle");
+return 0;
+}
+
+void _puts(char *str)
+{
+printf("%s\n", str);
+}
+
